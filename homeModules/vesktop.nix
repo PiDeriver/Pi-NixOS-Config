32302778319269b67ff524b1090a6e93aec569e0
PiDeriver <pideriver@gmail.com>
2025-11-05 19:52:26 -0800

    Added theme and plugin settings for Vesktop. Also added files for custom cursor.

--- homeModules/vesktop.nix
+++ homeModules/vesktop.nix
@@ -2,5 +2,53 @@
 {
   programs.vesktop = {
     enable = true;
+    vencord.settings ={
+      enabledThemes = ../theme/ClearVision-v7-BetterDiscord.theme.css
+      plugins: {
+        ChatInputButtonAPI: {
+          enabled = true;
+        };
+        BlurNSFW: {
+          enabled = true;
+          blurAmount = 10;
+        };
+        FixYoutubeEmbeds: {
+          enabled = true;
+        };
+        OnePingPerDM: {
+          enabled = true;
+          channelToAffect = "both_dms";
+          allowMentions = true;
+          allowEveryone = true;
+        };
+        SendTimestamps: {
+          enabled = true;
+          replaceMessageContents = true;
+        };
+        ShowConnections: {
+          enabled = true;
+          iconSize = 32;
+          iconSpacing = 1;
+        };
+        SpotifyCrack: {
+          enabled = true;
+          noSpotifyAutoPause = true;
+          keepSpotifyActivityOnIdle = true;
+        };
+        WebKeybinds: {
+          enabled = true;
+        };
+        WebScreenShareFixes: {
+          enabled = true;
+        };
+        YoutubeAdblock: {
+          enabled = true;
+        };
+        VolumeBooster: {
+          enabled = true;
+          multiplier = 1;
+        };
+      };
+    };
   };   
 }
