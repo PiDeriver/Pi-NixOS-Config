3de7d25311f6ffb533693046003b1b3ea914a71b
PiDeriver <pideriver@gmail.com>
2026-08-22 12:26:15 -0700

    Fixed the updateRollback command in zsh.nix

--- homeModules/zsh.nix
+++ homeModules/zsh.nix
@@ -8,7 +8,7 @@
       ".." = "cd ..";
       update = "(cd $HOME/.dotfiles/; sudo nixos-rebuild switch --impure --flake . --option cores 8 --option max-jobs 6 2>&1 | grep -E -v 'Added input|follows|github:')";
       updateReboot = "(cd $HOME/.dotfiles/; sudo nixos-rebuild boot --impure --flake . --option cores 8 --option max-jobs 6 2>&1 | grep -E -v 'Added input|follows|github:')";
-      updateRollback = "(cd $HOME/.dotfiles/; sudo nixos-rebuild switch --rollback)";
+      updateRollback = "(cd $HOME/.dotfiles/; sudo nixos-rebuild switch --rollback --impure --flake . --option cores 8 --option max-jobs 6 2>&1 | grep -E -v 'Added input|follows|github:')";
       cfetch = "(cd $HOME/.dotfiles/cfetch-main/; ./cfetch)";
       disks = "sudo -E gparted";
       bms-init = "$HOME/.dotfiles/endlessDream/BMS-init.sh";
